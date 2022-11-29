db7203973c16490b169d1d03f40932859b1c0f66
Gisna Fauzian Dermawan <gisnafauziande198@gmail.com>
2022-11-29 13:42:07 +0700

    creatting Shopping List

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.module';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent {
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomato', 5),
+  ];
 
 }
+
+// export class ShoppingListComponent implements OnInit{
+//   ingredients=[];
+
+//   constructor(){}
+
+//   ngOnInit(){
+
+//   }
+// }
