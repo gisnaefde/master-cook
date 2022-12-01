dcfa6be6b1abd83120264c6abe4c5c8b13a13655
Gisna Fauzian Dermawan <gisnafauziande198@gmail.com>
2022-12-01 14:44:25 +0700

    Adding Ingredients to Recipes

--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -1,4 +1,5 @@
 import {EventEmitter} from '@angular/core';
+import { Ingredient } from '../shared/ingredient.module';
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
@@ -6,8 +7,22 @@ export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes:Recipe[] = [ //ini dibuat privat
-    new Recipe ('A test Recipe One', 'This is a simply Test One','https://cdn.loveandlemons.com/wp-content/uploads/2020/03/bean-recipes-1-803x1024.jpg' ),
-    new Recipe ('A test Recipe Two', 'This is a simply Test Two','https://cdn.loveandlemons.com/wp-content/uploads/2020/03/bean-recipes-1-803x1024.jpg' )
+    new Recipe ('A test Recipe One',
+    'This is a simply Test One',
+    'https://cdn.loveandlemons.com/wp-content/uploads/2020/03/bean-recipes-1-803x1024.jpg',
+    [
+      new Ingredient('Meat' ,1),
+      new Ingredient('French Fries' ,20),
+    ]
+     ),
+    new Recipe ('A test Recipe Two',
+    'This is a simply Test Two',
+    'https://cdn.loveandlemons.com/wp-content/uploads/2020/03/bean-recipes-1-803x1024.jpg',
+    [
+      new Ingredient ('Buns' , 2),
+      new Ingredient ('Meat' , 2),
+    ]
+     )
   ];
 
   getRecipes(){ //unutk mengembalikan array
