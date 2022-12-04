f174d94707d804e316857283bf793386de1c7b89
Gisna Fauzian Dermawan <gisnafauziande198@gmail.com>
2022-12-04 17:31:05 +0700

    Retrieving Route Parameters

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute , Params } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-edit.component.css']
 })
 export class RecipeEditComponent {
+
+  id : number;
+  editMode : boolean = false;
+
+  constructor(private route : ActivatedRoute){}
+
+  ngOnInit() {
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.id = +params['id'];
+          this.editMode = params['id'] != null; //unutk menentukan edit atau new
+        }
+      )
+  }
 
 }
